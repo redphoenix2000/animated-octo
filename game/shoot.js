b30ee630bb0dd2a9a439ea05a4ef5bfacacfedbf
Jean-Gabriel Mathe <jean-gabriel.mathe@epita.fr>
2021-09-22 20:10:34 +0200

    First commit

--- game/shoot.js
+++ game/shoot.js
@@ -81,19 +81,22 @@ function player_falling()
     var element = null;
 
     for (var i = 0; i < length; i++) {
-        element = noGround[i];
-
-        var tileX = (element[0]) | 0;
-        var tileY = (element[1]) | 0;
-        var mtileX = (element[0] + sizeOfTileX) | 0;
-        var mtileY = (element[1] + sizeOfTileY) | 0;
-
-        if ((x > tileX)
-            && (x < mtileX)
-            && (y > tileY) 
-            && (y < mtileY))
+        if (noGround[i]!= null)
         {
-           player1.dead();
+            element = noGround[i];
+
+            var tileX = (element[0]) | 0;
+            var tileY = (element[1]) | 0;
+            var mtileX = (element[0] + sizeOfTileX) | 0;
+            var mtileY = (element[1] + sizeOfTileY) | 0;
+
+            if ((x > tileX)
+                && (x < mtileX)
+                && (y > tileY) 
+                && (y < mtileY))
+            {
+                player1.dead();
+            }
         }
     }
 
